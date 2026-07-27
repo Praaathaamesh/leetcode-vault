914f416ef03527083fb6083375fc93ec63e4f9dc
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-07-27 19:55:19 +0530

    feat: cleared rem_ele python3

--- array/remove_element.py
+++ array/remove_element.py
@@ -1,12 +1,21 @@
 '''
 strategy to be used here is: 
-    - binary search: 
-        - left as leftmost idx and right as rightmost idx
-        --> while left is smaller right
-            --> calculate the middle index
-            --> if nums at mid is target; return mid, less than target add 1 to left, else minus 1 by right
-        --> return left
+    - Two pointers: 
+        - slow as a write pointer keeping the idx of next value to keep
+        --> fast pointer checks entire array
+            --> if not same as val, keep it and copy to slow, advance slow
+            --> if val; then skip, fast moves
+        --> return slow
 
 complexity:
-    - O(log n) time
+    - O(n) time; O(1) space complexity
 '''
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
