77382dc0abd3ff0f993bc0d98b2853ca6f018b5f
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-07-28 20:52:32 +0530

    feat: solved pascal_triangle python3

---
+++ array/pascals_triangle.py
@@ -0,0 +1,21 @@
+'''
+strategies to be used here:
+    - Build row by row
+
+complexity:
+    - O(n^2) time and space; here n is number of rows
+'''
+
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        triangle = []
+
+        for row_num in range(numRows):
+            row = [1] * (row_num + 1)
+
+            for j in range(1, row_num):
+                row[j] = triangle[row_num -1][j-1] + triangle[row_num -1][j]
+
+            triangle.append(row)
+
+        return triangle
