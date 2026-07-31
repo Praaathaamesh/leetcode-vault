edae101b144bf7c0fab620c9020c80e1b276284d
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-07-31 20:06:14 +0530

    fix: fixed logic  palindrome python3

--- maths/palindrome_number.py
+++ maths/palindrome_number.py
@@ -8,12 +8,13 @@ compplexity:
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0  and (x % 10 == 0 and x != 0):
+    # negative numbers and numbers ending in 0 (except 0 itself) can't be palindromes
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-
+        
         reverted = 0
         while x > reverted:
             reverted = reverted * 10 + x % 10
             x //= 10
-
+        
         return x == reverted or x == reverted // 10
