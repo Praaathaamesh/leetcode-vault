59f3a1d4c85a11996978cf6358525e643e8bcff8
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-08-02 20:09:34 +0530

    fix: fixed logic climbing_stairs python3

--- dynamic_porg/climbing_stairs.py
+++ dynamic_porg/climbing_stairs.py
@@ -11,7 +11,7 @@ class Solution:
         if n <= 2:
             return n
 
-        prev1, prev2 = 1, 2 
+        prev2, prev1 = 1, 2 
 
         for i in range(3, n + 1):
             current = prev1 + prev2
