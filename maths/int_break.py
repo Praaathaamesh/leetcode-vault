d27d2c1573348ff52c7c1372dbd49168e548c8c6
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-08-13 10:38:24 +0530

    feat: solved int_break python3

---
+++ maths/int_break.py
@@ -0,0 +1,21 @@
+'''
+strategy to be used here:
+    - math pattern ()
+
+complexity:
+    - O(1) time and space
+'''
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        if n % 3 == 0:
+            return 3 ** (n // 3)
+        elif n % 3 == 1:
+            return 3 ** (n // 3 - 1) * 4
+        else:
+            return 3 ** (n // 3) * 2
