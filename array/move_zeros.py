81d1f1b69b27d58695e6e0a06076fbc3618f82d2
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-08-17 13:20:39 +0530

    feat: solved move zeros python3

---
+++ array/move_zeros.py
@@ -0,0 +1,24 @@
+'''
+strategies to be used here:
+    - snowball approach
+
+complexity:
+    - O(n) time and O(1) space
+'''
+
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        snowball_size = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                snowball_size += 1
+            elif snowball_size > 0:
+                nums[i - snowball_size] = nums[i]
+                nums[i] = 0
+        
