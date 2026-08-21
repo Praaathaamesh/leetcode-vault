442449f75b2582a865c25d4aac08af86f44a99fb
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-08-21 17:39:54 +0530

    feat: solved add_ints python3

---
+++ maths/add_ints.py
@@ -0,0 +1,11 @@
+'''
+strategy to be used here:
+    - just add them bro
+
+complexity:
+    - O(1) time and space 
+'''
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
