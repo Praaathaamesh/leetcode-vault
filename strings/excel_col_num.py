ed6bb03709a7528c067b3d06c7b962669d815968
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-08-25 19:29:05 +0530

    feat/fix: change filename xlsx_col_title and solved xlsx_col_num python3

--- strings/excel_col_num.py
+++ strings/excel_col_num.py
@@ -1,18 +1,17 @@
 '''
 strategy to be used here:
-    -  ascii mapping
+    - formula for convert the title to num, no use list
 
 complexity:
-    - O(log n) time and O(n) space
+    - O(n) time and O(1) space
 '''
 
 class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
-        results = []
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
 
-        while columnNumber > 0:
-            columnNumber = columnNumber - 1
-            results.append(chr(ord('A') + columnNumber % 26))
-            columnNumber = columnNumber // 26
-        
-        return ''.join(reversed(results))
+        for char in columnTitle:
+            result = result * 26
+            result += ord(char) - ord('A') + 1
+
+        return result
